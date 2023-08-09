c1ad8d770bdb48ba8160e6f7cc309886a753a66e
Qev254 <kelvinkkaranja@gmail.com>
2023-08-09 21:20:59 +0300

    shortkode

--- seedchat-gpt.php
+++ seedchat-gpt.php
@@ -8,11 +8,15 @@
 
 // Enqueue styles and scripts
 function seedchat_gpt_enqueue_scripts() {
+    // Enqueue CSS file
     wp_enqueue_style('seedchat-gpt-style', plugin_dir_url(__FILE__) . 'assets/css/SeedChat-GPT.css');
+    
+    // Enqueue jQuery and JS file
     wp_enqueue_script('jquery');
     wp_enqueue_script('seedchat-gpt-script', plugin_dir_url(__FILE__) . 'assets/js/SeedChat-GPT.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'seedchat_gpt_enqueue_scripts');
+
 
 // Chatbot shortcode
 function seedchat_gpt_chatbot_shortcode() {
