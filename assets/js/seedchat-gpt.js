6cecbdc9b8b59b1955a512199c9f00efe6cb918e
Qev254 <kelvinkkaranja@gmail.com>
2023-08-09 22:12:19 +0300

    bearer

--- assets/js/seedchat-gpt.js
+++ assets/js/seedchat-gpt.js
@@ -34,7 +34,7 @@ jQuery(document).ready(function ($) {
                 }, {
                     headers: {
                         'Content-Type': 'application/json',
-                        'Authorization': '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', // Replace with your actual API key
+                        'Authorization': 'Bearer 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', // Replace with your actual API key
                     },
                 });
     
