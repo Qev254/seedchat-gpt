e936975e690cfcc26802cefb20cfd47250c4f284
Qev254 <kelvinkkaranja@gmail.com>
2023-08-09 22:18:36 +0300

    key updd

--- assets/js/seedchat-gpt.js
+++ assets/js/seedchat-gpt.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function ($) {
     // Define chatbot elements
-    var chatbotContainer = $('#seedchat-gpt-chatbot');
     var conversationArea = $('#seedchat-gpt-conversation');
     var messageInput = $('#seedchat-gpt-message');
     var submitButton = $('#seedchat-gpt-submit');
@@ -26,7 +25,7 @@ jQuery(document).ready(function ($) {
         if (userMessage !== '') {
             appendMessage(userMessage, 'user');
             messageInput.val('');
-    
+
             try {
                 // Make an API call to the ChatGPT API
                 const response = await axios.post('https://api.openai.com/v1/chat/completions', {
@@ -37,10 +36,10 @@ jQuery(document).ready(function ($) {
                         'Authorization': 'Bearer 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', // Replace with your actual API key
                     },
                 });
-    
+
                 // Get the bot's reply from the API response
                 const botReply = response.data.choices[0].message.content;
-    
+
                 // Display the bot's reply after a delay
                 setTimeout(function () {
                     appendMessage(botReply, 'bot');
@@ -50,7 +49,6 @@ jQuery(document).ready(function ($) {
             }
         }
     });
-    
 
     // Initialize the chatbot on page load
     initializeChatbot();
